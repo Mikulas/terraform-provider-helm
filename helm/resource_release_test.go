a9e2588e8f7435a581022a2fa3e68134a183cf9f
Alex Somesan <alex.somesan@gmail.com>
2019-03-19 21:36:02 +0100

    Remove leftover debug statement

--- helm/resource_release_test.go
+++ helm/resource_release_test.go
@@ -640,7 +640,6 @@ func deleteNamespace(t *testing.T, namespace string) {
 		t.Fatal("provider not properly initialized")
 	}
 
-	t.Logf("[DEBUG] Deleting namespace %q", namespace)
 	gracePeriodSeconds := int64(0)
 	deleteOptions := meta_v1.DeleteOptions{
 		GracePeriodSeconds: &gracePeriodSeconds,
